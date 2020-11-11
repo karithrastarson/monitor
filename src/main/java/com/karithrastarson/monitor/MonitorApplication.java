fb88c2794641b9ae5b2e6f3e47c218661c087130
karithrastarson <kari.thrastarson@agillic.com>
2020-11-11 22:44:23 +0100

    Spring initializer

---
+++ src/main/java/com/karithrastarson/monitor/MonitorApplication.java
@@ -0,0 +1,13 @@
+package com.karithrastarson.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitorApplication.class, args);
+	}
+
+}
